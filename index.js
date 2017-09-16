04fc7387a77722a872efa77d97084636264b4f02
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 18:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ var recipes = {
 //   return object
 // }
 
-function updateObjectWithKeyAndValue1(object, key, value) {
-  return Object.assign({}, object, { key: value })
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
 }
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-    object[key] = value
-    return object
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var clone = Object.assign({}, object);
+  delete clone[key];
+  return clone;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
