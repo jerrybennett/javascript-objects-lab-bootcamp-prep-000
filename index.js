21480fb8dd1df07f6f89ebd8cfaa402f4b8dde6b
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-16 18:26:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {
 
 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  // object[key] = value
+  // return object
   return Object.assign({}, object, { key: value })
 }
